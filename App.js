2b1a9f6e39e55292b8e620fc4ee09dbc54784db3
Alexis Piraux <alexispiraux@MBP-de-Alexis.home>
2020-05-11 16:32:54 +0200

    update

--- App.js
+++ App.js
@@ -1,63 +1,46 @@
-import * as React from 'react'
-import { Platform, StatusBar, StyleSheet, View } from 'react-native'
-import { SplashScreen } from 'expo'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator'
-import useLinking from './navigation/useLinking'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
+console.disableYellowBox = true;
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false)
-  const [initialNavigationState, setInitialNavigationState] = React.useState()
-  const containerRef = React.useRef()
-  const { getInitialState } = useLinking(containerRef)
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
 
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide()
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      spac_mono: require('./assets/fonts/SpaceMono-Regular.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
 
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState())
+  render(){
 
-        // Load fonts
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-      } finally {
-        setLoadingComplete(true)
-        SplashScreen.hide()
-      }
+    if (!this.state.isReady) {
+      return <AppLoading/>;
     }
 
-    loadResourcesAndDataAsync()
-  }, [])
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer
-          ref={containerRef}
-          initialState={initialNavigationState}
-        >
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+    return(
+        <NavigationContainer>
+          <Stack.Navigator headerMode="none">
+            <Stack.Screen name="Accueil" component={BottomTabNavigator}/>
           </Stack.Navigator>
         </NavigationContainer>
-      </View>
+
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-})
