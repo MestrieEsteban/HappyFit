8fceb33fb1c4b7431179e6273cdc3d3bdb10f5b7
Alexis Piraux <alexispiraux@MacBook-Pro-de-Alexis.local>
2020-07-07 02:17:09 +0200

    update

---
+++ css/style_connexion.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  border_title: {
+    flex: 0.1,
+  },
+  size_title: {
+    fontSize: 40,
+  },
+  border_icons: {
+    flex: 0.05,
+  },
+  border_input_1: {
+    flex: 0.2,
+  },
+  border_input_2: {
+    flex: 0.07,
+  },
+  input_back: {
+    backgroundColor: 'transparent',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+    width: 200,
+  },
+  input_text: {
+    fontSize: 15,
+  },
+  placement_btn_1: {
+    flex: 0.2,
+  },
+  border_btn_1: {
+    borderColor: '#3598DA',
+    backgroundColor: '#3598DA',
+    borderWidth: 1,
+    width: 150,
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text_btn_1: {
+    fontSize: 15,
+    color: 'white',
+    marginTop: 10,
+  },
+  placement_btn_2: {
+    flex: 0.05,
+  },
+  border_btn_2: {
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    width: 150,
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text_btn_2: {
+    fontSize: 15,
+    color: 'black',
+    marginTop: 10,
+  },
+})
--- screens/Inscription.js
+++ screens/Inscription.js
@@ -1,158 +1,53 @@
 import React, { Component } from 'react'
-import { Button, View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
 
-class InscriptionScreen extends Component {
-  constructor(props) {
-    super(props)
+import styles_home from '../css/style_home'
+import { MaterialIcons } from '@expo/vector-icons'
 
-    this.state = {
-      email: '',
-      password: '',
-    }
-  }
-
-  async _register() {
-    console.log(
-      'email:' + this.state.email + ', password:' + this.state.password
-    )
-
-    if (this.state.email != '' && this.state.password != '') {
-      await fetch('https://team-foster.herokuapp.com/api/users/register', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: this.state.email,
-          firstname: 'Mike', //this.state.firstname,
-          lastname: 'Horn', //this.state.lastname,
-          pass: this.state.password,
-          confirm_password: this.state.password, //this.state.confirm_password
-        }),
-      })
-        .then((response) => response.json())
-        .then((responseJson) => {
-          if (responseJson.status === 'success') {
-            this.setState({ message: JSON.stringify(responseJson.message) })
-            this.setState({
-              email: '',
-              firstname: '',
-              lastname: '',
-              password: '',
-              confirm_password: '',
-            })
-            alert('Votre inscriptions à été validé')
-            this.props.navigation.navigate('Home')
-          } else {
-            this.setState({ message: JSON.stringify(responseJson.error) })
-            alert(JSON.stringify(responseJson.error))
-            this.props.navigation.navigate('Home')
-          }
-        })
-    }
-  }
-
+export default class Inscription extends Component {
   render() {
+    const { navigation } = this.props
     return (
-      <View style={styles.container}>
-        <View style={styles.image1}>
-          <Image source={require('../assets/images/icon.jpg')} />
+      <View style={styles_home.container}>
+        <View style={styles_home.view_image}>
+          <MaterialIcons name="account-circle" size={24} color="black" />
         </View>
 
-        <View style={styles.text1}>
-          <Text style={{ textAlignVertical: 'center', textAlign: 'center' }}>
+        <View style={styles_home.placement_text}>
+          <Text>
+            Happy Fit c’est une méthode pour être{'\n'}
             {'\n'}
+            "heureux quel que soit son fit "{'\n'}
+            {'\n'}
+            Soit en s’amusant à notre cours de sport, soit en participant à un
+            de mes ateliers confiance en soi.{'\n'}
+            {'\n'}
+            ​Ce que nous voulons transmettre avec HappyFit : c’est que le sport
+            c’est avant tout du plaisir et ça reste le meilleur moyen de prendre
+            confiance en soi.
           </Text>
         </View>
-
-        <View>
-          <TextInput
-            onChangeText={(email) => this.setState({ email: email.trim() })}
-            style={{
-              marginLeft: 70,
-              marginRight: 70,
-              height: 50,
-              borderColor: '#000000',
-              borderWidth: 1,
-              paddingLeft: 5,
-            }}
-            placeholder="Email"
-          />
-          <Text>{'\n'}</Text>
-          <TextInput
-            onChangeText={(password) =>
-              this.setState({ password: password.trim() })
-            }
-            secureTextEntry={true}
-            style={{
-              marginLeft: 70,
-              marginRight: 70,
-              height: 50,
-              borderColor: '#000000',
-              borderWidth: 1,
-              paddingLeft: 5,
-            }}
-            placeholder="Mot de passe"
-          />
-        </View>
-
-        <View>
-          <Text style={{ textAlignVertical: 'center', textAlign: 'center' }}>
-            {'\n'}Accepter les conditons d'utilisations
-          </Text>
-        </View>
-
-        <View style={styles.button1}>
-          <Button
-            onPress={() => this.props.navigation.navigate('Connexion')}
-            title="Se connecter"
-            color="#000000"
-          />
-        </View>
-
-        <View style={styles.button2}>
-          <Button
-            onPress={() => {
-              this._register().done()
-            }}
-            title="S'inscrire"
-            color="#000000"
-          />
+        <View style={styles_home.border_container}></View>
+        <View style={styles_home.view_button}>
+          <View style={styles_home.button}>
+            <TouchableOpacity
+              style={styles_home.placement_button}
+              onPress={() => navigation.navigate('Introduction')}
+            >
+              <Text style={styles_home.placement_btn_text}>Inscription</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles_home.border_container}></View>
+          <View style={styles_home.button}>
+            <TouchableOpacity
+              style={styles_home.placement_button}
+              onPress={() => navigation.navigate('Connexion')}
+            >
+              <Text style={styles_home.placement_btn_text}>Connexion</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#FFFFFF',
-  },
-  image1: {
-    width: 140,
-    height: 200,
-    justifyContent: 'center',
-    marginTop: 20,
-    marginLeft: 50,
-  },
-  text1: {},
-  button1: {
-    height: 45,
-    borderRadius: 25,
-    justifyContent: 'center',
-    marginTop: 20,
-    alignItems: 'center',
-    backgroundColor: '#000000',
-  },
-  button2: {
-    height: 45,
-    borderRadius: 25,
-    justifyContent: 'center',
-    marginTop: 10,
-    alignItems: 'center',
-    backgroundColor: '#000000',
-  },
-})
-
-export default InscriptionScreen
